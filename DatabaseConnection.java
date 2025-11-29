aa2072c827b12f13622558087ea64dc1ab409ba6
sahasra-tekula <sahasratekula@gmail.com>
2025-11-29 07:12:20 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/exam_proctoring";
     private static final String USER = "root"; // your MySQL username
-    private static final String PASSWORD = "1234"; // your MySQL password
+    private static final String PASSWORD = "YOUR_DB_PASSWORD"; // your MySQL password
 
     public static Connection getConnection() throws SQLException {
         try {
